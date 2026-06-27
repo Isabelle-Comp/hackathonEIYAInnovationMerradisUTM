5f41c522ae30d4d8f1361dfa6d0fd62eaee4a6c2
Isabelle-Comp <compaoreisabelle65@gmail.com>
2026-06-27 16:02:12 +0000

    Hackathon MERRADIS UTM equipe EIYA Innovations code backend V1

--- src/main/java/com/hackthon/security/SecurityConfig.java
+++ src/main/java/com/hackthon/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://entraide.essitechgroup.com/" )); // Spécifiez les origines autorisées
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173" )); // Spécifiez les origines autorisées
         //configuration.setAllowedOrigins(Arrays.asList("https://entraide.essitechgroup.com/")); // Spécifiez les origines autorisées
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
